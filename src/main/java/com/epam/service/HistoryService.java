e9e7f823949d748946460a0d7d350fe4169733ea
Oliinyk <olijnikdmitro969@gmail.com>
2020-04-29 12:30:42 +0300

    add HistoryServiceImpl.getHistory

--- src/main/java/com/epam/service/HistoryService.java
+++ src/main/java/com/epam/service/HistoryService.java
@@ -1,8 +1,13 @@
 package com.epam.service;
 
 import com.epam.dto.BookedPlaceDto;
+import com.epam.dto.HistoryDto;
 import com.epam.dto.TrainDto;
+
+import java.util.List;
 
 public interface HistoryService {
     boolean saveTicket(BookedPlaceDto bookedPlaceDto, TrainDto trainDto, Long userId);
+
+    List<HistoryDto> getHistory(Long userId);
 }
--- src/main/java/com/epam/service/impl/HistoryServiceImpl.java
+++ src/main/java/com/epam/service/impl/HistoryServiceImpl.java
@@ -1,10 +1,16 @@
 package com.epam.service.impl;
 
 import com.epam.dao.HistoryDao;
+import com.epam.dao.builder.HistoryBuilder;
 import com.epam.dto.BookedPlaceDto;
+import com.epam.dto.HistoryDto;
 import com.epam.dto.TrainDto;
+import com.epam.dto.mapper.HistoryDtoMapper;
 import com.epam.entity.History;
 import com.epam.service.HistoryService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HistoryServiceImpl implements HistoryService {
     private HistoryDao historyDao;
@@ -23,5 +29,13 @@ public class HistoryServiceImpl implements HistoryService {
                         trainDto.getPrice(), userId
                 )
         );
+    }
+
+    @Override
+    public List<HistoryDto> getHistory(Long userId) {
+        return historyDao.getByFields(new HistoryBuilder(), userId)
+                .stream()
+                .map(new HistoryDtoMapper()::fromDtoToEntity)
+                .collect(Collectors.toList());
     }
 }
